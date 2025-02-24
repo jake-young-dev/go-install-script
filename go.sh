9a235ece4bb07758cef502534e67613c798c5e9d
unknown <17075259+jake-young-dev@users.noreply.github.com>
2025-02-24 18:17:38 -0500

    linking to avoid path additions

--- go.sh
+++ go.sh
@@ -54,7 +54,7 @@ echo "Creating GOPATH directories"
 
 GOPATH="${ACT_TOOLSDIRECTORY}/go"
 sudo mkdir -v -m 0777 -p "$GOPATH"
-# sudo mkdir -v -m 0777 -p /usr/local/go
+sudo mkdir -v -m 0777 -p /usr/local/go
 
 echo "Downloading go files for ${DL_VERSION} ${DL_ARCH}"
 
@@ -72,7 +72,8 @@ ls ${GOPATH}
 # sudo ln -s "${GO_PATH}/${DL_VERSION}/${DL_ARCH}/bin/*" /usr/bin
 # sudo ln -s "${GO_PATH}"/* /usr/local/go
 echo "Testing here"
-export PATH="$GOPATH/bin:$PATH"
+# export PATH="$GOPATH/bin:$PATH"
+sudo ln -s "${GO_PATH}/go"/* /usr/local/go
 go version
 
 GLOBAL_GO_CMD_VERSION=$(go version)
