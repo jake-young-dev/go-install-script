dc25e3a81bba26bf3362b0c94d3620ec4b56a038
jake <17075259+jake-young-dev@users.noreply.github.com>
2025-02-26 00:15:48 -0500

    new/gocmds (#8)
    moving to env variables instead of cmd line inputs
    adding new input to install go commands

--- go.sh
+++ go.sh
@@ -3,7 +3,7 @@
 echo "Starting Golang install"
 
 #validate version and architecture
-echo "Finding go version"
+echo "Parsing go version"
 #looking for go version in go.mod file, only checking up to minor version ignoring patch value to allow for stable versions
 # -P uses perl syntax
 DL_VERSION_RAW="$(grep "^go [0-9]+.[0-9]+" go.mod -P)"
@@ -12,14 +12,17 @@ if [[ -z "$DL_VERSION_RAW" ]]; then
   exit 1
 fi
 echo "Found go version: ${DL_VERSION_RAW}"
+
+#checking for required input
 #"." is default arch value meaning required field was not set
-if [[ "$1" == "." ]]; then
+if [[ "$GO_INSTALL_ARCH" == "." ]]; then
   echo "FATAL: Missing required Linux archictecture parameter"
   exit 1
 fi
-DL_ARCH=$1
+DL_ARCH=$GO_INSTALL_ARCH
 DL_VSPL=( $DL_VERSION_RAW )
 DL_VERSION="${DL_VSPL[1]}"
+
 #ensure we have patch-level version, if not add 0 for stable releases
 # -P uses perl syntax
 DL_VERSION_PAD_CHECK="$(grep "^go [0-9]+.[0-9]+.[0-9]+" go.mod -P)"
@@ -28,31 +31,28 @@ if [[ -z "$DL_VERSION_PAD_CHECK" ]]; then
   echo "Fixing version number to ${DL_VERSION}"
 fi
 
-#check if go is already present before starting install process
+echo "Checking if go is already installed"
+#check if go is already present before starting install process and delete files if purge input is set
 # -v writes string that indicates command or command path to output, prevents command not found error
 GO_CHECK=$(command -v go)
 if [[ "$GO_CHECK" ]]; then
-  #if overwrite flag is set remove old go files
-  if [[ "$2" == "yes" ]]; then
+  #if purge flag is set remove old go files
+  if [[ "$GO_INSTALL_PURGE" == "yes" ]]; then
+    echo "Removing old go versions"
     sudo rm -r /usr/bin/go 
     sudo rm -r /usr/bin/gofmt
   else
-    echo "FATAL: Go version {$GO_CHECK} already installed, set purge to 'yes' if you wish to update installed version"
+    echo "FATAL: Go is already installed, set purge to 'yes' if you wish to update installed version"
     exit 1
   fi
 fi
+echo "Ready for install"
 
-#handle go path
-GOPATH="${ACT_TOOLSDIRECTORY}/go/${DL_VERSION}/${DL_ARCH}"
-echo "Creating GOPATH directories"
-#create go DIRs
-# -v prints out each dir created
-# -m set chmod value for created dirs
-# -p create parent dirs as needed without errors
-sudo mkdir -v -m 0777 -p "$GOPATH"
+echo "Downloading go files for ${DL_VERSION}/${DL_ARCH}"
+PATH_FOR_FILES=/usr/local/go
+PATH_FOR_TAR=/usr/local
+sudo mkdir -v -m 0777 -p "$PATH_FOR_FILES"
 
-#download and extract go files
-echo "Downloading go files for ${DL_VERSION} with ${DL_ARCH}"
 #wget
 # -q quiets output
 # O- outputs file data to pipe instead of file
@@ -61,42 +61,43 @@ echo "Downloading go files for ${DL_VERSION} with ${DL_ARCH}"
 # -x extract files
 # -f extract from "file"
 # - extract from pipe as a file
-# --strip-components=1 strip first leading component from file name on extraction
 # -C change to GOPATH directory
-if sudo wget -qO- "https://golang.org/dl/go${DL_VERSION}.linux-${DL_ARCH}.tar.gz" | sudo tar -zxf - --strip-components=1 -C "$GOPATH"; then
-  echo "Downloaded go files"
+if sudo wget -qO- "https://golang.org/dl/go${DL_VERSION}.linux-${DL_ARCH}.tar.gz" | sudo tar -zxf - -C "$PATH_FOR_TAR"; then
+  echo "Files downloaded"
 else
-  echo "FATAL: Unable to download and extract go files"
-  exit 1
+  echo "FATAL: Unable to download and extract files"
 fi
 
-#link bin directories for system command
-# -s symbolic link
-echo "Creating symbolic link for go command"
-sudo ln -s "$GOPATH/bin"/* /usr/bin
+echo "Setting path for go command"
+export PATH=$PATH:$PATH_FOR_FILES/bin
+echo "$PATH_FOR_FILES/bin" >> "$GITHUB_PATH" #set action path too
 
-#grab installed go version
-DL_GO_CMD_VERSION=$("$GOPATH/bin/go" version)
-if [[ -z "$DL_GO_CMD_VERSION" ]]; then
-  echo "FATAL: Failed to install go files"
-  exit 1
-else
-  echo "Go setup for {$DL_GO_CMD_VERSION}"
-fi
-
-#grab go version using go command
+#grab go version to test the go command
 GLOBAL_GO_CMD_VERSION=$(go version)
 if [[ -z "$GLOBAL_GO_CMD_VERSION" ]]; then
   echo "FATAL: Failed to register go command"
   exit 1
-fi
-
-#ensure global go version matches the requested installed version
-if [[ "$DL_GO_CMD_VERSION" == "$GLOBAL_GO_CMD_VERSION" ]]; then
-  echo "Go command successfully setup for: {$GLOBAL_GO_CMD_VERSION}"
 else
-  echo "FATAL: Global go version does not match installed version, is go already installed?"
-  exit 1
+  echo "Go command setup for ${GLOBAL_GO_CMD_VERSION}"
 fi
 
-echo "Golang successfully installed"
+#adding GOPATH to path to support go commands
+echo "Setting path to allow for commands installed by go"
+GP=$(go env GOPATH)/bin
+export PATH=$PATH:$GP
+echo "$GP" >> "$GITHUB_PATH"
+
+#if commands input is set pull the input and install them all using 'go install'
+if [[ "$GO_INSTALL_COMMANDS" != "." ]]; then # "." is used as a default
+  #allows for multiple cmds using |
+  INPUT_ARR=( $GO_INSTALL_COMMANDS )
+  for i in "${INPUT_ARR[@]}"; do
+      echo "Installing go command from ${i}"
+      if go install ${i}; then
+        echo "Command setup for ${i}"
+      else
+        echo "FATAL: Unable to install ${i} with go"
+        exit 1
+      fi
+  done
+fi
